d2dee55dcb7fa7b87974ac1135df2003270f6ca1
SoundarAathira <sounder.rla@gmail.com>
2022-11-06 19:39:42 +0530

    second push

--- src/test/java/com/git/GITClass.java
+++ src/test/java/com/git/GITClass.java
@@ -1,5 +1,23 @@
 package com.git;
 
 public class GITClass {
+	public void test1() {
+		System.out.println();
+
+	}
+	public void test2() {
+		System.out.println();
+
+	}
+	public void test3() {
+		System.out.println();
+
+	}
+	public void test4() {
+		System.out.println();
+
+	}
+	
+	
 
 }
